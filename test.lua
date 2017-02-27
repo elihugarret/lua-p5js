9992bf68fd04c8d1bb88377a9171596d028a467a
Elihu Garrett <pmaekb@hotmail.com>
2017-02-27 01:38:42 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,38 +1,45 @@
--- https://www.openprocessing.org/sketch/402537
-local elements
-      local tot = 80
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script src="js/p5.js"></script>
+    <script src="js/p5.dom.js"></script>
+    <script src="js/p5.sound.js"></script>
+    <script type="text/lua">
+
+      local p5 = window
+
+      local x = 70
+      local speed = 1
+      local radius = 90
+      local angle = 0
 
       function p5.setup()
-        p5:createCanvas(800, 600)
-        init()
+        p5:createCanvas(800, 500)
+        p5:smooth()
+        p5:noStroke()
       end
 
       function p5.draw()
-        for i = 1, #elements do
-          local el = elements[i]
-          el:move()
-          el:bounce()
-        end
-        for j = 0, #elements do
-          local el = elements[i]
-          for k = i + 1, #elements do
-            local el2 = elements[k]
-            el:onOverlap(el2)
-          end
-        end
-      end
-
-      function p5.mousePressed()
-        init()
-      end
-
-      function init()
-        p5:background(255)
-        elements = {}
-        for i = 1, tot do
-          local s = math.random(0, 2)
-          local col = p5:color(182 * s, 103 * s, 13 * s, 2)
-          local EL = Element.new(p5:random(p5.width), p5.height/2, p5:random(40, 60), col)
-          table.insert(elements, EL)
+        p5:fill(0, 4)
+        p5:stroke(200, 20)
+        angle = angle - .005
+        p5:translate(x, p5.height / 1.8)
+        p5:translate(y, p5.width)
+        p5:rotate(p5:noise(angle) + math.sin(angle))
+        p5:scale(2, 2, 2)
+        p5:triangle(-50, -50, -50, -50, 30, 3)
+        p5:triangle(-50, -50, -50, -50, 90, 30)
+        p5:triangle(-50, 0, -20, -20, 110, 2)
+        x = x + speed
+        if x > p5.width + radius then
+          x = -radius
         end
       end
+    </script>
+  </head>
+  <body>
+  </body>
+  <script src="js/starlight.js" data-run-script-tags></script>
+</html>
